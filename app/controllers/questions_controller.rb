c3a58e4bcd193a5130ba0ddf931bbe9555dfb2ac
Icorrea7 <isacorrea07@gmail.com>
2018-03-22 19:51:55 -0500

    nav-bar

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,24 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
   end
 
   def show
+    @question = Question.find(params[:id])
+    @question.answers
+    @question.answers.new
   end
 
   def edit
+    @question = Question.find(params[:id])
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:name, :description, :vote)
   end
 end
